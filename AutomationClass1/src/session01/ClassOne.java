a0ca72062b8ae14a3a4bdad8d2bab4e9792548f3
Siddharth Anyan <116862306+SiddharthAnyan@users.noreply.github.com>
2025-02-28 15:42:25 +0530

    Add files via upload

---
+++ AutomationClass1/src/session01/ClassOne.java
@@ -0,0 +1,44 @@
+package session01;
+
+public class ClassOne {
+	
+	public static void institute() 
+	{
+		System.out.println("I am learning automation Testing from Gautam Testing Academy");
+	}
+	
+	public static void address() 
+	{
+		System.out.println("I am living in Noida");
+	}
+	
+	public static void skills() 
+	{
+		System.out.println("My skills are manual testing, API testing by using postman, Performance Testing by using jmeter");
+	}
+	
+
+	public static void main(String[] args) {
+		System.out.println("I am Siddharth");
+		System.out.println("I am working as QA Engineer in ABC Company");
+		System.out.println("I have more than 2 years of experience");
+		System.out.println("Currently I am Learning Automation Testing");
+		institute();
+		address();
+		skills();
+		project();
+		greet();
+
+}
+	
+	public static void project() 
+	{
+		System.out.println("Currently working on crush & Crust project");
+	}
+	
+	public static void greet() 
+	{
+		System.out.println("Thank you");
+	}
+
+}
